36ccb43d87a611d72fffc8294989650246a08e7f
Anna Yang <yanganna68@gmail.com>
2018-02-16 10:06:10 -0500

    DONE

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180212163552) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "challenges", force: :cascade do |t|
     t.integer "user_id"
     t.integer "rival_id"
